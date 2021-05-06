f2ed239b9af3cdaeeca16d0b1beda3ff1d685839
greenart <tkfkd7887@naver.com>
2021-05-07 00:55:58 +0900

    4

--- src/main/java/com/greenart/MyHome/model/User.java
+++ src/main/java/com/greenart/MyHome/model/User.java
@@ -39,7 +39,7 @@ public class User {
     private List<Role> roles = new ArrayList<>();
 
 //  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-//  @JsonIgnore
+  @OneToMany(mappedBy = "user")     //, fetch = FetchType.LAZY
+  @JsonIgnore
   private List<Board> boards = new ArrayList<>();
 }
