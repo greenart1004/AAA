f8bf7d470ec4262116a4d112d2c9a292393b887c
greenart <tkfkd7887@naver.com>
2021-05-07 12:10:11 +0900

    1

--- src/main/java/com/greenart/MyHome/model/User.java
+++ src/main/java/com/greenart/MyHome/model/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -36,7 +37,7 @@ public class User {
     private List<Role> roles = new ArrayList<>();
 
 	//  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-	 // @OneToMany(mappedBy = "user")     //, fetch = FetchType.LAZY
-	//  @JsonIgnore
+     @OneToMany(mappedBy = "user")     //, fetch = FetchType.LAZY
+	// @JsonIgnore
 	 private List<Board> boards = new ArrayList<>();
 }
